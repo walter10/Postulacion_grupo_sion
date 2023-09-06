a05f85237cf49d8c075084cee860b810c2689b1d
wwantelo <wwantelo@gmail.com>
2023-09-06 09:49:18 -0400

    cambios y ajustes de pantalla 	modified:   src/routes/customer.js 	modified:   src/views/customers.ejs 	new file:   src/views/login.ejs

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -3,7 +3,7 @@ const customerController = require('../controllers/customerControllers');
 const express = require('express');
 const router = express.Router();
 
-
+//router.get ('/',loadLogin.charge);
 router.get('/',customerController.list);
 router.post('/add',customerController.save);
 
