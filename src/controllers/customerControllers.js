6a66ceeccfa5e3ee81f352f55bb4def64545011b
wwantelo <wwantelo@gmail.com>
2023-09-06 10:59:43 -0400

    agregacion de la funcion editar (incompleta faltan cambios por mejorar) 	modified:   src/controllers/customerControllers.js 	modified:   src/routes/customer.js 	new file:   src/views/customer_edith.ejs

--- src/controllers/customerControllers.js
+++ src/controllers/customerControllers.js
@@ -18,9 +18,33 @@ controller.list = (req, res)=> {
 
 controller.save = (req,res) => {
     console.log(req.body);
-    res.send('works');
+    const data = req.body;
+    req.getConnection((err,conn) => {
+        conn.query("INSERT INTO customer set ?",[data], (err, customer)=>{            
+            res.redirect('/');   
+        });
+    })
     
 };
+
+controller.edit = (req,res) => {
+    const {id} = req.params;
+    req.getConnection((err,conn)=> {
+        conn.query('SELECT * FROM customer WHERE id = ?',[id],(err,customer) => {
+            res.render("customers_edit", { customer: result[0] });
+
+        });
+    })
+
+
+}
+
+
+controller.delete =(req, res) => {
+    const {id} = req.params;
+    console.log('click edith');
+    res('work')
+}
 
 
 module.exports = controller;
--- src/routes/customer.js
+++ src/routes/customer.js
@@ -6,5 +6,7 @@ const router = express.Router();
 //router.get ('/',loadLogin.charge);
 router.get('/',customerController.list);
 router.post('/add',customerController.save);
+router.get('/delete/:id',customerController.delete);
+router.get('/update/:id',customerController.edit);
 
 module.exports = router;
